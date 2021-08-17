e264bfa61a8af7ce23dc82a794824381e2830a3a
alefari <ale.jf09@gmail.com>
2021-08-17 17:46:01 -0400

    Transfiere lógica de auth a servicio, y añade manejo de errores

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
-import firebase from 'firebase/app';
-import { StringDecoder } from 'string_decoder';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,73 +14,50 @@ export class LoginComponent implements OnInit {
   errorMessage: string = null;
   isLogin = true;
 
-  constructor(public auth: AngularFireAuth, private afs: AngularFirestore) { }
+  constructor(public auth: AngularFireAuth, private afs: AngularFirestore, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   onSignUpEmail(authForm: NgForm) {
-    this.auth.createUserWithEmailAndPassword(authForm.value.email, authForm.value.password)
-    .then((credential) => {
-      this.addNewUserDB(credential.user.uid, credential.user.email, authForm.value.nombre, authForm.value.aplellido,);
-    });
-  }
-
-  addNewUserDB(uid: string, email: string, nombre: string, apellido: string) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`usuarios/${uid}`);
-      const data = {
-      uid: uid,
-      email: email,
-      nombre: nombre,
-      apellido: apellido,
-      roles: ['user']
-    }
-    console.log(userRef.set(data, {merge: true }));
-  }
-
-  onLoginEmail(authForm: NgForm) {
-    this.auth.signInWithEmailAndPassword(authForm.value.email, authForm.value.password).then(
+    this.authService.signUpEmail(
+      authForm.value.email,
+      authForm.value.password,
+      authForm.value.nombre,
+      authForm.value.apellido
+    ).then(
       res => {
-        this.errorMessage = null;
         console.log(res)
-        this.auth.user.subscribe(user => {
-          console.log(user.displayName)
-        })
-      },
-      error => {
-        switch (error.code) {
-          case 'auth/invalid-email':
-          case 'auth/user-not-found':
-            this.errorMessage = "El email ingresado no es válido.";
-            break;
-          case 'auth/user-disabled':
-          case 'auth/too-many-requests':
-            this.errorMessage = "Su usuario ha sido inhabilitado temporalmente, espere unos minutos o contacte un administrador.";
-            break;
-          case 'auth/wrong-password':
-            this.errorMessage = "Las credenciales ingresadas no coindicen, asegúrese de que son correctas en intente de nuevo.";
-            break;
-          default:
-            this.errorMessage = "Se ha encontrado un error, verifique sus credenciales e intente de nuevo.";
-            break;
-        }
+        this.errorMessage = null
       }
+    ).catch(
+      err => this.errorMessage = err
     );
   }
 
+  onLoginEmail(authForm: NgForm) {
+    this.authService.loginEmail(
+      authForm.value.email,
+      authForm.value.password
+      ).then(
+        res => {
+          console.log(res)
+          this.errorMessage = null
+        }
+      ).catch(
+        err => this.errorMessage = err
+      );
+  }
+
   onLoginGoogle() {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(
+    this.authService.loginGoogle().then(
       res => {
-        this.addNewUserDB(
-          res.user.uid,
-          res.additionalUserInfo.profile['email'],
-          res.additionalUserInfo.profile['given_name'],
-          res.additionalUserInfo.profile['family_name'] )
-      },
-      error => {
-        console.log(error)
+        console.log(res)
+        this.errorMessage = null
       }
-    )
+    ).catch(
+      err => this.errorMessage = err
+    );
   }
 
   logout() {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,121 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import firebase from 'firebase/app';
+import { throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, private afs: AngularFirestore) { }
+
+  async signUpEmail(email: string, password: string, nombre: string, apellido: string): Promise<string> {
+    return this.auth.createUserWithEmailAndPassword(email, password).then(
+      credential => {
+        this.addNewUserDB(credential.user.uid, credential.user.email, nombre, apellido);
+        return "Success";
+      },
+      error => {
+        throw this.handleError(error.code);
+      });
+  }
+
+  async loginEmail(email: string, password: string): Promise<string> {
+    return this.auth.signInWithEmailAndPassword(email, password).then(
+      res => {
+        this.auth.user.subscribe(user => {
+          console.log(user.displayName)
+        })
+        return "Success";
+      },
+      error => {
+        throw this.handleError(error.code);
+      }
+    );
+  }
+
+  async loginGoogle() {
+    return this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(
+      res => {
+        if(res.additionalUserInfo.isNewUser == true) {
+          this.addNewUserDB(
+            res.user.uid,
+            res.additionalUserInfo.profile['email'],
+            res.additionalUserInfo.profile['given_name'],
+            res.additionalUserInfo.profile['family_name']
+          ).then(
+            res => console.log(res),
+            err => console.log(err)
+          )
+        }
+      },
+      error => {
+        throw this.handleError(error.code)
+      }
+    )
+  }
+
+  private async addNewUserDB(uid: string, email: string, nombre: string, apellido: string) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`usuarios/${uid}`);
+      const data = {
+      uid: uid,
+      email: email,
+      nombre: nombre,
+      apellido: apellido,
+      roles: ['user']
+    }
+    userRef.set(data, {merge: true }).catch(err => console.log(err))
+  }
+
+  handleError(errorCode: string): string {
+    let errorMessage = null;
+        switch (errorCode) {
+          case 'auth/weak-password':
+            errorMessage = "La contraseña debe tener al menos 6 caracteres.";
+          break;
+
+          case 'auth/email-already-in-use':
+            errorMessage = "Este email ya está en uso, utilice otra dirección de correo o contacte un administrador.";
+          break;
+
+          case 'auth/invalid-email':
+            errorMessage = "El email ingresado no es válido, utilice otra dirección de correo.";
+          break;
+
+          case 'auth/invalid-email':
+          case 'auth/user-not-found':
+            errorMessage = "El email ingresado no es válido.";
+          break;
+
+          case 'auth/user-disabled':
+          case 'auth/too-many-requests':
+            errorMessage = "Su usuario ha sido inhabilitado temporalmente, espere unos minutos o contacte un administrador.";
+          break;
+
+          case 'auth/wrong-password':
+            errorMessage = "Las credenciales ingresadas no coindicen, asegúrese de que son correctas en intente de nuevo.";
+          break;
+
+          case 'auth/account-exists-with-different-credential':
+            errorMessage = "Ya existe una cuenta con estas credenciales, pruebe iniciar sesión.";
+          break;
+
+          case 'auth/cancelled-popup-request':
+            errorMessage = "Solo se permite una ventana emergente de inicio de sesión a la vez.";
+          break;
+
+          case 'auth/popup-blocked':
+            errorMessage = "Se ha bloqueado la ventana emergente de inicio de sesión, por favor habilite las ventanas emergentes en su navegador e intente de nuevo.";
+          break;
+
+          case 'auth/popup-closed-by-user':
+            errorMessage = "Debe completar el inicio de sesión antes de cerrar la ventana emergente.";
+          break;
+
+          default:
+            errorMessage = "Se ha encontrado un error, verifique los datos ingresados e intente de nuevo.";
+        }
+      return errorMessage
+  }
 }
