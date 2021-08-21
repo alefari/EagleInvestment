a568c3654666f4c7d652a1804509dc87e65e934c
alefari <ale.jf09@gmail.com>
2021-08-20 20:54:13 -0400

    Añade componente favoritos al routing y su link de acceso en dashboard

--- src/app/user-module/user-routing.module.ts
+++ src/app/user-module/user-routing.module.ts
@@ -4,12 +4,14 @@ import { UserPropiedadesComponent } from './components/user-propiedades/user-pro
 import { UserBaseComponent } from './components/user-base/user-base.component';
 import { UserPerfilComponent } from './components/user-perfil/user-perfil.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
 
 const routes: Routes = [
   {
     path: '', component: UserBaseComponent,
     children: [
       {path: '', redirectTo: 'propiedades'},
+      {path: 'favoritos', component: FavoritosComponent},
       {path: 'propiedades', component: UserPropiedadesComponent},
       {path: 'perfil', component: UserPerfilComponent},
       {path: 'admin', component: AdminComponent},
