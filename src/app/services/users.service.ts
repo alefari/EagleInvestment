1c9b8166926eede8c77386e56c98acb94e4807f9
Alejandro Fariña <ale.jf09@gmail.com>
2021-09-08 20:34:45 -0400

    Agrega funcion get usuarios en servicio users y data binding en tabla usuarios menu admin

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  usersColeccionRef: AngularFirestoreCollection<User>;
+  users: Observable<User[]>;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore) {
+    this.usersColeccionRef = afs.collection<User>('usuarios');
+    this.users = this.usersColeccionRef.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return {id, ...data};
+      }))
+    )
+  }
+
+  getUsers() {
+    return this.users;
+  }
 
   updateUser(updatedUser: User) {
     let userDoc = this.afs.doc<User>(`usuarios/${updatedUser.uid}`);
--- src/app/user-module/components/admin/admin.component.ts
+++ src/app/user-module/components/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  usuarios: User[];
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.usersService.getUsers().subscribe(users => {
+      this.usuarios = users.sort((a, b) => (a.nombre > b.nombre ? 1 : -1))
+    })
   }
 
 }
