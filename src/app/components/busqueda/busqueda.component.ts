4f383ee31254da5f66dee5b5b1619b09901731b4
Alejandro Fariña <ale.jf09@gmail.com>
2021-10-20 20:11:57 -0400

    Funcionalidad de ver inmuebles en componente busqueda

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faBed, faBath, faRuler, faCar } from '@fortawesome/free-solid-svg-icons'
+import { Inmueble } from 'src/app/models/inmueble.model';
+import { InmueblesService } from 'src/app/services/inmuebles.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -12,10 +14,14 @@ export class BusquedaComponent implements OnInit {
   faRuler = faRuler;
   faCar = faCar;
   arrayPrueba = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+  inmuebles: Inmueble[];
 
-  constructor() { }
+  constructor(private servicioInmuebles: InmueblesService) { }
 
   ngOnInit(): void {
+    this.servicioInmuebles.getInmuebles().subscribe(inmuebles => {
+      this.inmuebles = inmuebles.sort((a, b) => (a.zona > b.zona ? 1 : -1))
+    })
   }
 
 
